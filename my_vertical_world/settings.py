928a23d517ce7a79a972178831bb5b0e65e45976
szalashaska <kamilpetryniak@gmail.com>
2022-11-07 16:08:29 +0100

    railway config

--- my_vertical_world/settings.py
+++ my_vertical_world/settings.py
@@ -14,15 +14,21 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-64@h7nxpeb6dz%#m$*x7muh!3(g^!$r(5zw^c&k$l%8und7cw6'
+
+# SECRET_KEY = 'django-insecure-64@h7nxpeb6dz%#m$*x7muh!3(g^!$r(5zw^c&k$l%8und7cw6'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
