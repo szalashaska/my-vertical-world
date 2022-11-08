468af6ac0ab9bc9f3ec290e2f6217b39cc4e2e96
szalashaska <kamilpetryniak@gmail.com>
2022-11-08 13:52:51 +0100

    added whitenoise middleware

--- my_vertical_world/settings.py
+++ my_vertical_world/settings.py
@@ -106,6 +106,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_vertical_world.urls'
